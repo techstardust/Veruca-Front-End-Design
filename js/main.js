1b9506538f64a0f1f0fc0f40e038d4087579e7f8
LiamOliver <liamoliver5@gmail.com>
2015-11-29 21:25:23 -0800

    Updated Front End

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 //Scroll Animation for About
 $(function() {
     //caches a jQuery object containing the header element
@@ -6,40 +5,8 @@ $(function() {
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
 
-        if (scroll >= 2700) {
+        if (scroll >= 500) {
             animate.removeClass('display-none').addClass("display");
         } 
     });
 });
-
-
-
-//Scroll To
-$(".scroll").click(function(event){		
-	event.preventDefault();
-	$("html,body").animate({scrollTop:$(this.hash).offset().top}, 300)
-});
-
-
-//Scroll Spy Refresh
-$('#navbar').scrollspy()
-
-
-
-//Scroll To Top
-$(document).ready(function(){
- 
-        $(window).scroll(function(){
-            if ($(this).scrollTop() > 100) {
-                $('.scrollToTop').fadeIn();
-            } else {
-                $('.scrollToTop').fadeOut();
-            }
-        });
- 
-        $('.scrollToTop').click(function(){
-            $("html, body").animate({ scrollTop: 0 }, 600);
-            return false;
-        });
- 
-    });
